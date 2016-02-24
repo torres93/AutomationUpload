6ea2947d048f7aa3f1f5941f7ecea08f4df48e8a
torres93 <luis.ernesto.torres.herrera@gmail.com>
2016-02-24 10:53:26 -0600

    inicio del proyecto

---
+++ AutomationUpload/AutomationUpload/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("AutomationUpload")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("AutomationUpload")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("3a970cf8-3feb-452a-a608-07d4c6c20108")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationUpload/AutomationUpload/js/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("AutomationUpload", ['ngRoute', 'ngMaterial', 'ngMessages']);
+app.config(
+	function ($routeProvider) {
+	    $routeProvider.
+		when('/', {
+		    templateUrl: 'views/login.html'
+		})
+     .otherwise({
+         redirectTo: '/'
+     });
+	})
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('indigo')
+      .accentPalette('pink')
+      .warnPalette('red')
+    ;
+})
