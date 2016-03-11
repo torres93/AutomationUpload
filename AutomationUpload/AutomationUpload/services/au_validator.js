f0d28c45c68c72e382b389f3c1bec5eaa8b49141
NovaDvlop <cesar.rodriguezc.1992@gmail.com>
2016-03-11 14:21:29 -0600

    erwrwerwe

---
+++ AutomationUpload/AutomationUpload/services/au_validator.js
@@ -0,0 +1,19 @@
+﻿
+
+app.service("$au_validator", function ($http) {
+
+    this.fnValidateLength = function (modelo) {
+        alert("");
+        var m = { id_modelo: modelo }
+        $http.post("wsApp.asmx/getWorkTable", m).success(function ($response) {
+            console.log($response.d);
+        });
+    }
+
+    this.InsertValues = function()
+    {
+        $http.post("wsApp.asmx/putWorkTable", m).success(function ($response) {
+            console.log($response.d);
+        });
+    }
+});
