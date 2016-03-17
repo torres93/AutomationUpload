dcf04268c27f12bc9d60d845b5f8f81e6182d0fd
Angel1912 <ingenieromagel2@gmail.com>
2016-03-17 13:57:10 -0600

    Actualizacion al controlador NCVControl.js y wsApp

--- AutomationUpload/AutomationUpload/controllers/NCCtrl.js
+++ AutomationUpload/AutomationUpload/controllers/NCCtrl.js
@@ -8,96 +8,25 @@ app.controller("userCtrl", function ($scope, $http, authUsers, sesionesControl, 
         $scope.modelos = $response;
     });
 
-    $scope.validaVista = function (catalogoV1, catalogoV2)
-    {
-        var m = JSON.stringify({ vista: catalogoV1, tabla: catalogoV2 ,insTbl:''});
-        $http.post("wsApp.asmx/validaCargaVista", m).success(function ($response) {
-            
-            if ($response.d== "nomms") {
-                $mdDialog.show(
-                             $mdDialog.alert()
-                             .clickOutsideToClose(true)
-                             .title('Campos de tablas diferentes')
-                             .content()
-                             .ok('Aceptar')
-                             )
-            } else {
-                $scope.validarV = JSON.parse($response.d);
-
-                if ($scope.validarV[0].cont == '0' && $scope.validarV[0].existentrows == '0') {
-                            $mdDialog.show(
-                              $mdDialog.alert()
-                              .clickOutsideToClose(true)
-                              .title('Valicación completa')
-                              .content()
-                              .ok('Aceptar')
-                              )
-                            var m = JSON.stringify({ insTbl: catalogoV1, vista: '', tabla: '' });
-                            $http.post("wsApp.asmx/validaCargaVista", m).success(function ($response) {
-                            });
-
-                } else if ($scope.validarV[0].cont != '0') {
-                            $mdDialog.show(
-                              $mdDialog.alert()
-                              .clickOutsideToClose(true)
-                              .title('Existen '+$scope.validarV[0].cont+' lineas Repetidos en ' +catalogoV1 )
-                              .content()
-                              .ok('Aceptar')
-                              )
-                } else if ($scope.validarV[0].existentrows != '0') {
-                    $mdDialog.show(
-                      $mdDialog.alert()
-                      .clickOutsideToClose(true)
-                      .title('Existen ' + $scope.validarV[0].existentrows + ' lineas Repetidos en ' + catalogoV2)
-                      .content()
-                      .ok('Aceptar')
-                      )
-                } else {
-                    $mdDialog.show(
-                      $mdDialog.alert()
-                      .clickOutsideToClose(true)
-                      .title('Existen lineas Repetidos en ' + catalogoV1 +' y '+catalogoV2)
-                      .content()
-                      .ok('Aceptar')
-                      )
-                }
-            }
-       
-        });
-    }
-   
-    $scope.configModeloVista = function (modelo)
-    {
+    $scope.configModeloVista = function (modelo) {
         var res;
         for (var i = 0; i < $scope.modelos.length; i++) {
             if ($scope.modelos[i].nombre == modelo) {
-                 res = $scope.modelos[i].id_modelo;
+                res = $scope.modelos[i].id_modelo;
             }
         }
         $scope.id_modelo = res;
         var m = JSON.stringify({ modelo: res });
-        $http.post("wsApp.asmx/getEncuestas",m).success(function ($response) {
+        $http.post("wsApp.asmx/getEncuestas", m).success(function ($response) {
             //console.log($response);
             $scope.encuestas = JSON.parse($response.d);
         })
-        $http.post("wsApp.asmx/getCatalogosModelo",m).success(function ($response) {
+        $http.post("wsApp.asmx/getCatalogosModelo", m).success(function ($response) {
             //console.log($response);
             $scope.catalogos = JSON.parse($response.d);
         });
     }
 
-    $scope.configVista = function (encuesta) {
-
-        for (var i = 0; i < $scope.encuestas.length; i++) {
-            if ($scope.encuestas[i].nombre == encuesta) {
-                modelo = $scope.encuestas[i].id_modelo;
-            }
-        };
-        modeloJson = JSON.stringify({ model: modelo });
-        $http.post("wsApp.asmx/getCampoCatalogo", modeloJson).success(function ($response) {
-            $scope.campos = JSON.parse($response.d);
-        });
-    }
     $scope.selected = [];
     $scope.toggle = function (campos, list) {
         var idx = list.indexOf(campos.nombre);
@@ -113,10 +42,8 @@ app.controller("NCCtrl", ["$scope", "$http", "$au_validator", "fileUpload", func
     $scope.pathFile = "";
     $scope.path = "";
     $scope.data;
-    $scope.fnValidate = function()
-    {
-        if($scope.data!=null)
-        {
+    $scope.fnValidate = function () {
+        if ($scope.data != null) {
             var n = $scope.$parent.id_modelo
             $validator.fnValidateLength(n);
         }
@@ -132,14 +59,12 @@ app.controller("NCCtrl", ["$scope", "$http", "$au_validator", "fileUpload", func
             document.getElementById("tableContainer").removeAttribute("hidden");
         });
     };
-    $scope.NotePath = function()
-    {
-     
-        $scope.$apply(function()
-        {
+    $scope.NotePath = function () {
+
+        $scope.$apply(function () {
             fullName = $('#fileSearch').val();
             shortName = fullName.match(/[^\/\\]+$/);
-            $scope.pathShort= $('#filepath').value = shortName;
+            $scope.pathShort = $('#filepath').value = shortName;
             $scope.path = fullName;
         })
 
--- AutomationUpload/AutomationUpload/controllers/NCVControl.js
+++ AutomationUpload/AutomationUpload/controllers/NCVControl.js
@@ -1,5 +1,106 @@
 ﻿var app = angular.module("AutomationUpload");
-app.controller("NCVCtrl", function ($scope, $http, authUsers, sesionesControl) {
+app.controller("NCVCtrl", function ($scope, $http, authUsers, sesionesControl, $mdDialog) {
+    $scope.vistaValidada = true;
+    $scope.validaVista = function (catalogoV1, catalogoV2) {
+        var m = JSON.stringify({ vista: catalogoV1, tabla: catalogoV2 });
+        $http.post("wsApp.asmx/validaCargaVista", m).success(function ($response) {
 
+            if ($response.d == "exception") {
+                $mdDialog.show(
+                             $mdDialog.alert()
+                             .clickOutsideToClose(true)
+                             .title('Ocurrio Algun Error!')
+                             .content(' Verifica que las 2 tablas tengan la misma estructura')
+                             .ok('Aceptar')
+                             )
+            } else {
+                $scope.validarV = JSON.parse($response.d);
+
+
+
+
+
+                if ($scope.validarV[0].cont == '0' && $scope.validarV[0].existentrows == '0') {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                      .clickOutsideToClose(true)
+                      .title('Valicación completa')
+                      .content('se valido que: 1) la vista no tiene duplicados 2) la vista no tiene valores ya cargados en la tabla de cifras')
+                      .ok('Aceptar')
+                      )
+                    $scope.vistaValidada = false;
+
+                    //$http.post("wsApp.asmx/validaCargaVista", m).success(function ($response) {
+                    //    console.log($response.d);
+
+                    //});
+
+
+
+                } else if ($scope.validarV[0].cont != '0') {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                      .clickOutsideToClose(true)
+                      .title('La vista' + catalogoV1 + ' tiene ' + $scope.validarV[0].cont + ' lineas Repetidas')
+                      .content()
+                      .ok('Aceptar')
+                      )
+                } else if ($scope.validarV[0].existentrows != '0') {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                      .clickOutsideToClose(true)
+                      .title($scope.validarV[0].existentrows + ' lineas ya se encuentran registradas en ' + catalogoV2)
+                      .content()
+                      .ok('Aceptar')
+                      )
+                } else {
+                    $mdDialog.show(
+                      $mdDialog.alert()
+                      .clickOutsideToClose(true)
+                      .title('Existen lineas Repetidos en ' + catalogoV1 + ' y ' + catalogoV2)
+                      .content()
+                      .ok('Aceptar')
+                      )
+                }
+            }
+
+        });
+    }
+    $scope.configVista = function (encuesta) {
+
+        for (var i = 0; i < $scope.encuestas.length; i++) {
+            if ($scope.encuestas[i].nombre == encuesta) {
+                modelo = $scope.encuestas[i].id_modelo;
+            }
+        };
+        modeloJson = JSON.stringify({ model: modelo });
+        $http.post("wsApp.asmx/getCampoCatalogo", modeloJson).success(function ($response) {
+            $scope.campos = JSON.parse($response.d);
+        });
+    }
+    $scope.replicaVista = function () {
+        $http.post("wsApp.asmx/replicaVista", modeloJson).success(function ($response) {
+            if ($response.d == "") {
+
+                $mdDialog.show(
+                             $mdDialog.alert()
+                             .clickOutsideToClose(true)
+                             .title('Ocurrio Algun Error!')
+                             .content(' La replica se realizó satisfactoriamente')
+                             .ok('Aceptar')
+                             )
+            } else {
+                $mdDialog.show(
+                             $mdDialog.alert()
+                             .clickOutsideToClose(true)
+                             .title('Ocurrio Algun Error!')
+                             .content(' Ocurrio algun error al realizar la replica de la informacion ')
+                             .ok('Aceptar')
+                             )
+                    $scope.vistaValidada = true;
+
+            }
+        });
+    }
 
 })
